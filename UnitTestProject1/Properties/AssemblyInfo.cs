77f538059a0210c583e5b3d83341c4be52d65092
412-02 <daratkareva771@gmail.com>
2024-05-06 09:33:54 +0300

    Сессия 1

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52142fac-e47b-4bde-a8b1-161822466381")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int expRes = -3;
+            int actRes = Class1.LibraryTestMetod(-1, -2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int expRes = int.MaxValue;
+            int actRes = Class1.LibraryTestMetod(int.MinValue, -1);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int expRes = 3;
+            int actRes = Class1.LibraryTestMetod(1, 2);
+            Assert.AreEqual(expRes, actRes, "Сложение не работает");
+        }
+    }
+}
